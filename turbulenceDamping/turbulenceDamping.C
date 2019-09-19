a1fa0596c38294fdd2affc1b3d304e90497b4412
wenyuan-fan <wf@kth.se>
2019-09-19 15:51:14 +0200

    change the layout

--- turbulenceDamping/turbulenceDamping.C
+++ turbulenceDamping/turbulenceDamping.C
@@ -52,124 +52,127 @@ volScalarField::Internal Foam::fv::turbulenceDamping::calculateSource
     const label fieldi
 )
 {
-            // read properties for each phase
-            const dictionary& transportProperties = mesh().lookupObject<IOdictionary>
-            (
-             "transportProperties"
-            );
+    // read properties for each phase
+    const dictionary& transportProperties = mesh().lookupObject<IOdictionary>
+    (
+        "transportProperties"
+    );
 
-            const word phase1Name_(wordList(transportProperties.lookup("phases"))[0]);
-            const word phase2Name_(wordList(transportProperties.lookup("phases"))[1]);
+    const word phase1Name_(wordList(transportProperties.lookup("phases"))[0]);
+    const word phase2Name_(wordList(transportProperties.lookup("phases"))[1]);
 
-            const dictionary& phase1_ = transportProperties.subDict(phase1Name_);
-            const dictionary& phase2_ = transportProperties.subDict(phase2Name_);
+    const dictionary& phase1_ = transportProperties.subDict(phase1Name_);
+    const dictionary& phase2_ = transportProperties.subDict(phase2Name_);
 
-            const dimensionedScalar& rho1_ = phase1_.lookup("rho");
-            const dimensionedScalar& rho2_ = phase2_.lookup("rho");
+    const dimensionedScalar& rho1_ = phase1_.lookup("rho");
+    const dimensionedScalar& rho2_ = phase2_.lookup("rho");
 
-            const dimensionedScalar& nu1_ = phase1_.lookup("nu");
-            const dimensionedScalar& nu2_ = phase2_.lookup("nu");
-
-
-            const volScalarField& Alpha = mesh().lookupObject<volScalarField>("alpha." + phase1Name_);
-            const volVectorField grad_Alpha = fvc::grad(Alpha);
-            const volScalarField grad_Alpha_mag = mag(grad_Alpha);
-
-            scalar beta = 0.075;
+    const dimensionedScalar& nu1_ = phase1_.lookup("nu");
+    const dimensionedScalar& nu2_ = phase2_.lookup("nu");
 
 
-            // calculate interfacial area density
-            volScalarField::Internal A1 = 2.0*Alpha*grad_Alpha_mag;
-            volScalarField::Internal A2 = 2.0*(1.0-Alpha)*grad_Alpha_mag;
+    const volScalarField& Alpha =
+        mesh().lookupObject<volScalarField>("alpha." + phase1Name_);
+
+    const volVectorField grad_Alpha = fvc::grad(Alpha);
+    const volScalarField grad_Alpha_mag = mag(grad_Alpha);
+
+    scalar beta = 0.075;
 
 
-            // calculate the inverse of the length scale
-            const volScalarField::Internal& V = mesh_.V();
+    // calculate interfacial area density
+    volScalarField::Internal A1 = 2.0*Alpha*grad_Alpha_mag;
+    volScalarField::Internal A2 = 2.0*(1.0-Alpha)*grad_Alpha_mag;
 
-            volScalarField oneByDn
-            (
-            IOobject
-            (
+
+    // calculate the inverse of the length scale
+    const volScalarField::Internal& V = mesh_.V();
+
+    volScalarField oneByDn
+    (
+        IOobject
+        (
             "oneByDn",
             mesh_.time().timeName(),
             mesh_,
             IOobject::NO_READ,
             IOobject::NO_WRITE
-            ),
-            mesh_,
-            dimensionedScalar("oneByDn",dimensionSet(0,-1,0,0,0,0,0),0.0)
-            );
+        ),
+        mesh_,
+        dimensionedScalar("oneByDn", dimless/dimLength, 0.0)
+        );
 
-            if (lengthScale_ == "FA")
+    if (lengthScale_ == "FA")
+    {
+        forAll (oneByDn, celli)
+        {
+            if (grad_Alpha_mag[celli] > SMALL)
             {
-                forAll(oneByDn, celli)
+                scalar projectedArea = 0.0;
+                const cell& c = mesh_.cells()[celli];
+
+                forAll(c, facej)
                 {
-                    if (grad_Alpha_mag[celli] > SMALL)
-                    {
-                        scalar projectedArea = 0.0;
-                        const cell& c = mesh_.cells()[celli];
-                        forAll(c, facej)
-                        {
-                            projectedArea += mag(grad_Alpha[celli] & mesh_.Sf()[c[facej]]);
-                        }
-
-                        oneByDn[celli] = 0.5*projectedArea/V[celli]/grad_Alpha_mag[celli];
-                    }
-                }
-            }
-
-            else if (lengthScale_ == "cubicRoot")
-            {
-                oneByDn.ref() = pow(V,-1.0/3.0);
-            }
-
-
-            // calculate separate damping terms
-            volScalarField::Internal coeffs = 36.0*sqr(B_)/beta*pow(oneByDn, 3.0);
-            volScalarField::Internal source1 = coeffs*A1*rho1_*sqr(nu1_);
-            volScalarField::Internal source2 = coeffs*A2*rho2_*sqr(nu2_);
-
-            // calculate the total damping term
-            dimensionedScalar heavy("heavy", dimless, 0.0);
-
-            volScalarField::Internal source = 0.0 * source1;
-
-            if (dampingTreatment_ == "heavyNegative")
-            {
-                if (rho1_ > rho2_)
-                {
-                    heavy = - rho2_/rho1_*sqr(nu2_)/sqr(nu1_);
-
-                    source = source1*heavy + source2;
+                    projectedArea += mag(grad_Alpha[celli] & mesh_.Sf()[c[facej]]);
                 }
 
-                else
-                {
-                    heavy = - rho1_/rho2_*sqr(nu1_)/sqr(nu2_);
-
-                    source = source1 + source2*heavy;
-                }   
+                oneByDn[celli] = 0.5*projectedArea/V[celli]/grad_Alpha_mag[celli];
             }
+        }
+    }
 
-            else if (dampingTreatment_ == "heavyZero")
-            {
-                if (rho1_ > rho2_)
-                {
-                    source = source2;
-                }
+    else if (lengthScale_ == "cubicRoot")
+    {
+        oneByDn.ref() = pow(V,-1.0/3.0);
+    }
 
-                else
-                {
-                    source = source1;
-                }   
-            }
 
-            else if (dampingTreatment_ == "symmetric")
-            {
-                source = source1 + source2;   
-            }
+    // calculate separate damping terms
+    volScalarField::Internal coeffs = 36.0*sqr(B_)/beta*pow(oneByDn, 3.0);
+    volScalarField::Internal source1 = coeffs*A1*rho1_*sqr(nu1_);
+    volScalarField::Internal source2 = coeffs*A2*rho2_*sqr(nu2_);
 
-            return source;
+    // calculate the total damping term
+    dimensionedScalar heavy("heavy", dimless, 0.0);
+
+    volScalarField::Internal source = 0.0 * source1;
+
+    if (dampingTreatment_ == "heavyNegative")
+    {
+        if (rho1_ > rho2_)
+        {
+            heavy = - rho2_/rho1_*sqr(nu2_)/sqr(nu1_);
+
+            source = source1*heavy + source2;
+        }
+
+        else
+        {
+            heavy = - rho1_/rho2_*sqr(nu1_)/sqr(nu2_);
+
+            source = source1 + source2*heavy;
+        }   
+    }
+
+    else if (dampingTreatment_ == "heavyZero")
+    {
+        if (rho1_ > rho2_)
+        {
+            source = source2;
+        }
+
+        else
+        {
+            source = source1;
+        }   
+    }
+
+    else if (dampingTreatment_ == "symmetric")
+    {
+        source = source1 + source2;   
+    }
+
+    return source;
 }
 
 
@@ -187,7 +190,10 @@ Foam::fv::turbulenceDamping::turbulenceDamping
     option(sourceName, modelType, dict, mesh),
     B_("B", dimless, coeffs_.lookup("B")),
     lengthScale_(coeffs_.lookupOrDefault<word>("lengthScale", "FA")),
-    dampingTreatment_(coeffs_.lookupOrDefault<word>("dampingTreatment", "heavyNagative"))
+    dampingTreatment_
+    (
+        coeffs_.lookupOrDefault<word>("dampingTreatment", "heavyNagative")
+    )
 {
     coeffs_.lookup("fields") >> fieldNames_;
 
@@ -210,8 +216,12 @@ void Foam::fv::turbulenceDamping::addSup
 )
 {
     const volScalarField& Rho = mesh().lookupObject<volScalarField>("rho");
-    const volScalarField& omega = mesh().lookupObject<volScalarField>(fieldNames_[fieldi]);
+
+    const volScalarField& omega =
+        mesh().lookupObject<volScalarField>(fieldNames_[fieldi]);
+
     volScalarField::Internal source = calculateSource(eqn, fieldi)/omega/Rho;
+
     eqn += fvm::Sp(source, omega);
 }
 
@@ -223,8 +233,11 @@ void Foam::fv::turbulenceDamping::addSup
     const label fieldi
 )
 {
-    const volScalarField& omega = mesh().lookupObject<volScalarField>(fieldNames_[fieldi]);
+    const volScalarField& omega =
+        mesh().lookupObject<volScalarField>(fieldNames_[fieldi]);
+
     volScalarField::Internal source = calculateSource(eqn, fieldi)/omega;
+
     eqn += fvm::Sp(source, omega);
 }
 
